b96b63f32eb4aaaaacc3d0b379b098a8ff97e4b6
Peter Walsh <peterwalsh0701@gmail.com>
2025-02-18 23:48:54 +0000

    Inital Commit with acs py file creation and provided monitoring added

---
+++ acs_ass1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+'''
+
+- Peter Walsh - 20098070 - Year 4 Automation Engineering - Created @ 18/02/2025 -
+
+Please see github readme.md for notes on this assignment:
+https://github.com/PetWalsh007/ACS_Assignment_1
+
+'''
+
+
+import boto3
+import logging
+import time
+import random
+import sys
+
+
+
+
+
+
+def create_ec2_instance():
+    pass
+
+
+def create_s3_bucket():
+    pass
+
+
+
+
+
+# Main function to call the above functions 
+
+def main():
+    create_ec2_instance()
+    create_s3_bucket()
+    pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
